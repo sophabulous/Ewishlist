a1a5eeeb16cc5dde0f099ec2e8e71142852367a1
Yushan Lin <yushanlin96@gmail.com>
2018-11-05 19:10:05 -0500

    Created template database classes and methods

--- backend/ProductService/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ backend/ProductService/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -3,7 +3,9 @@ package io.swagger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan(basePackages = { "io.swagger", "io.swagger.api" })
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
