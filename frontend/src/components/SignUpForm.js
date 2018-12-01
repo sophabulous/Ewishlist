c4b19a4658fe2aed59f64202c439ab4420c4424d
Yushan Lin <yushanlin96@gmail.com>
2018-12-01 00:22:49 -0500

    api call to put user

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {
   withRouter
 } from 'react-router-dom';
@@ -32,14 +33,39 @@ class SignUpForm extends React.Component {
           confirmpass: event.target.value
         }
       ]
-
     });
   }
 
   handleSubmit(event) {
-    //alert('Sign up finished for: ' + this.state.username);
+    // alert('Sign up finished for: ' + this.state.username);
     event.preventDefault();
-    this.props.history.push('/list');
+    const user = event.target.username.value
+    const pass = event.target.password.value
+    const email = event.target.email.value
+
+    const userreq = {
+      username: user,
+      password: pass,
+      email: email
+    };
+    fetch('http://localhost:8081/users/manage', {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userreq)
+
+      })
+     .then((data) => {
+      }, (error) => {
+        if (error) {
+          // handle error here
+          console.log(error)
+        }
+      });
+     //redirect to login page
+    this.props.history.push('/');
   }
 
   render() {
@@ -48,19 +74,19 @@ class SignUpForm extends React.Component {
       <form className="SignUpForm" onSubmit={this.handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" value={this.state.Person.username} onChange={this.handleNameChange} />
+          <input name="username" type="text" value={this.state.Person.username} onChange={this.handleNameChange} />
         </label>
         <label>
           <p>Email</p>
-          <input type="text" value={this.state.Person.email} onChange={this.handleChange} />
+          <input name="email" type="text" value={this.state.Person.email} onChange={this.handleChange} />
         </label>
         <label>
           <p>Password</p>
-          <input type="Password" value={this.state.Person.password} onChange={this.handleChange} />
+          <input name="password" type="Password" value={this.state.Person.password} onChange={this.handleChange} />
         </label>
         <label>
           <p>Confirm Password</p>
-          <input type="Password" value={this.state.Person.confirmpass} onChange={this.handleChange} />
+          <input name="confirmpass" type="Password" value={this.state.Person.confirmpass} onChange={this.handleChange} />
         </label>
         <div className="submitBtn">
           <input type="submit" value="Sign me Up!" />
