a6605fb8b4d20b89767ad60d85ed87c1fc9d5495
Gurleen Kaur <gurleen.kaur@mail.utoronto.ca>
2018-12-06 09:29:14 -0500

    Revert "Revert "Revert "Adding the frontend & backend connection for Tracking a product"""
    This reverts commit 4ea95d7117de8d7b5dbf00f9230b423378cf4afe.

--- frontend/src/pages/TrackingPage.js
+++ frontend/src/pages/TrackingPage.js
@@ -8,9 +8,6 @@ class TrackingPage extends Component {
 
   constructor(props) {
     super(props);
-    //user login token
-    const string = localStorage.getItem("token")
-    const token = JSON.parse(string)
     this.state = {
       query: '',
     };
@@ -27,40 +24,9 @@ class TrackingPage extends Component {
 
   handleTrack(event) {
     event.preventDefault();
-    //this.props.history.push('/confirm');
+    this.props.history.push('/confirm');
     //alert('yes we did the push');
-    const string = localStorage.getItem("token")
-    const token = JSON.parse(string)
-    const query = this.search.value
-    //console.log([query, token]);
-    //userreq to be passed to backend to add the prodcut for the user
-    const userreq = {
-      url: query,
-      loginToken: token
-    }
-    console.log(JSON.stringify(userreq));
 
-    fetch('http://localhost:9090/tracking/all', {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type' : 'application/json',
-      },
-      body: JSON.stringify(userreq)
-    })
-
-    .then(response => response.json())
-    .then((response) => {
-      console.log(response)
-    }, (error) => {
-      if(error) {
-        //handle error here
-        console.log(error);
-      }
-    });
-    //once added the product for tracking take back to the listpage
-    //this.props.history.pushState(null, '/list');
-    this.props.history.push('/list')
   }
 
     render () {
